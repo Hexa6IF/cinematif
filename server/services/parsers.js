7de6d36b95ff9e3749ca8f769a3bd2e4070ac2a8
sadsithalokuge <slokuge97@gmail.com>
2019-11-13 11:42:41 +0100

    Fixed error when no results

--- server/services/parsers.js
+++ server/services/parsers.js
@@ -16,13 +16,15 @@ const getDirectorsActors = (results) => {
 
 exports.cleanFilm = (results) => {
     const maps = getDirectorsActors(results)
-    results = [results[0]]
-    results[0].idact = undefined
-    results[0].iddirect = undefined
-    results[0].directname = undefined
-    results[0].actorname = undefined
-    results[0].actors = Array.from(maps.actorMap.values())
-    results[0].directors = Array.from(maps.directorMap.values())
+    if (results && results.length) {
+        results = [results[0]]
+        results[0].idact = undefined
+        results[0].iddirect = undefined
+        results[0].directname = undefined
+        results[0].actorname = undefined
+        results[0].actors = Array.from(maps.actorMap.values())
+        results[0].directors = Array.from(maps.directorMap.values())
+    }
     return results
 }
 
@@ -38,42 +40,3 @@ const getFilms = (results) => {
     });
     return { directorMap, actorMap }
 }
-
-exports.cleanFilm = (results) => {
-    const maps = getDirectorsActors(results)
-    results = [results[0]]
-    results[0].idact = undefined
-    results[0].iddirect = undefined
-    results[0].directname = undefined
-    results[0].actorname = undefined
-    results[0].actors = Array.from(maps.actorMap.values())
-    results[0].directors = Array.from(maps.directorMap.values())
-    return results
-}
-
-const getDirectorsActors = (results) => {
-    const directorMap = new Map()
-    const actorMap = new Map()
-    results.forEach(result => {
-        const directorKey = result.iddirect.value
-        const director = { id: result.iddirect, name: result.directname }
-        const actorKey = result.idact.value
-        const actor = { id: result.idact, name: result.actorname }
-        directorMap.set(directorKey, director)
-        actorMap.set(actorKey, actor)
-    });
-    return { directorMap, actorMap }
-}
-
-exports.cleanFilm = (results) => {
-    const maps = getDirectorsActors(results)
-    results = [results[0]]
-    results[0].idact = undefined
-    results[0].iddirect = undefined
-    results[0].directname = undefined
-    results[0].actorname = undefined
-    results[0].actors = Array.from(maps.actorMap.values())
-    results[0].directors = Array.from(maps.directorMap.values())
-    return results
-}
-
