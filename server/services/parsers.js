19e5268da1072456abbf4121f51761ec73fd11a5
hakimaab <55960458+hakimaab@users.noreply.github.com>
2019-11-15 08:42:37 +0100

    js ? a merge

--- server/services/parsers.js
+++ server/services/parsers.js
@@ -16,13 +16,27 @@ const getDirectorsActors = (results) => {
 
 exports.cleanFilm = (results) => {
     const maps = getDirectorsActors(results)
-    results = [results[0]]
-    results[0].idact = undefined
-    results[0].iddirect = undefined
-    results[0].directname = undefined
-    results[0].actorname = undefined
-    results[0].actors = Array.from(maps.actorMap.values())
-    results[0].directors = Array.from(maps.directorMap.values())
+    if (results && results.length) {
+        results = [results[0]]
+        results[0].idact = undefined
+        results[0].iddirect = undefined
+        results[0].directname = undefined
+        results[0].actorname = undefined
+        results[0].actors = Array.from(maps.actorMap.values())
+        results[0].directors = Array.from(maps.directorMap.values())
+    }
     return results
 }
 
+const getFilms = (results) => {
+    const filmsMap = new Map()
+    results.forEach(result => {
+        const directorKey = result.iddirect.value
+        const director = { id: result.iddirect, name: result.directname }
+        const actorKey = result.idact.value
+        const actor = { id: result.idact, name: result.actorname }
+        directorMap.set(directorKey, director)
+        actorMap.set(actorKey, actor)
+    });
+    return { directorMap, actorMap }
+}
