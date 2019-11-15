41bd6f8bdab96f70e447fa5684d8f547f76f6226
Arteox <ung.louis@hotmail.fr>
2019-11-15 09:55:02 +0100

    after clicking on a movie, redirects to movie.html with the id as a query string

--- client/public/request.js
+++ client/public/request.js
@@ -1,55 +1,64 @@
-let serverUrl = 'http://localhost:5000/api';
+const serverUrl = 'http://localhost:5000/api';
+const clientUrl = 'http://localhost:5000';
 
-function getMovies(movieSearch)
-{
+function getMovies(movieSearch) {
     movieSearch = movieSearch.toLowerCase();
-    let queryParams = {
-        title : movieSearch
+    const queryParams = {
+        title: movieSearch
     }
 
-    let searchUrl = '/search';
-    if (typeof movieSearch === 'string'){
-        $.getJSON(serverUrl+searchUrl, queryParams, function( responseData ) {
+    let searchUrl = serverUrl + '/search';
+    if (typeof movieSearch === 'string') {
+        $.getJSON(searchUrl, queryParams, function (responseData) {
             renderSearchResult(responseData);
         });
-    }
-    else {
+    } else {
         console.error(movieSearch);
         throw Error('getMovies : argument is not a string');
     }
 }
 
-function renderSearchResult (responseData) {
+function renderSearchResult(responseData) {
     let arrayBindings = responseData.results.bindings;
     let list = document.getElementById("movie-search-results");
     list.innerHTML = "";
-    let movieDetailUrl = '/detail/film/';
-    for (let i = 0; i < arrayBindings.length; i++){
+    const movieDetailUrl = '/movie.html';
+    for (let i = 0; i < arrayBindings.length; i++) {
         let movie = document.createElement("li");
         let movieDetail = document.createElement("a");
-        movieDetail.href = serverUrl + movieDetailUrl + arrayBindings[i].id.value;
+        movieDetail.href = clientUrl + movieDetailUrl + '?id=' + arrayBindings[i].id.value;
         movieDetail.appendChild(document.createTextNode(arrayBindings[i].title.value));
         movie.appendChild(movieDetail);
         list.appendChild(movie);
     }
 }
 
-function getMovie(movieIdentifier)
-//identifier could be a name or something else, to be decided
-{
-    let movieUrl = serverUrl+'/movie/'+movieIdentifier;
-    $.getJSON(movieUrl+movieIdentifier, function( responseData ) {
-        //TODO : serialize data
-        return responseData;
-    });
+function getMovieDetails(movieId) {
+    const movieUrl = serverUrl + '/detail/film/' + movieId;
+    try {
+        $.getJSON(movieUrl, function (responseData) {
+            renderMovieDetails(responseData);
+        });
+    } catch (e) {
+        throw Error(e);
+    }
 }
 
-function getActor(actorIdentifier)
-//identifier could be a name or something else, to be decided
-{
-    let movieUrl = serverUrl+'/actor/'+actorIdentifier;
-    $.getJSON(movieUrl+actorIdentifier, function( responseData ) {
-        //TODO : serialize data
-        return responseData;
-    });
+function renderMovieDetails(responseData) {
+    let arrayBindings = responseData.results.bindings;
+    console.log(arrayBindings);
 }
+
+function getActor(actorId)
+{
+    const actorUrl = serverUrl + '/detail/actor/' + actorId;
+    try {
+        $.getJSON(actorUrl, function (responseData) {
+            renderActorDetails();
+        });
+    } catch (e) {
+        throw Error(e);
+    }
+}
+
+function renderActorDetails() {}
